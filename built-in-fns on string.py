005c371cd339a97d27df17945a6cc885b274e455
Akansha0211 <49263297+Akansha0211@users.noreply.github.com>
2019-04-07 23:06:29 +0530

    Add files via upload

---
+++ built-in-fns on string.py
@@ -0,0 +1,8 @@
+'''Use of str.title,str.join,str.split,str.replace built-in-functions with a string'''
+a=input("Enter a string")
+print(a.title())
+print(" ".join(a))
+print(a.split("a"))
+print("".join(reversed(a)))
+print("-".join(["Akansha","Bharadwaj"]))
+print(a.replace("A","H"))
